7e486beb77887f8e1c68eb860782530d9dde3676
YanaLugina <yana4961@gmail.com>
2020-12-09 23:24:04 +1000

    giving data from context between screens

---
+++ android/app/src/main/kotlin/com/example/context_without_constructor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.context_without_constructor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: FirstHome(),
+    routes: {
+      '/first': (context) => FirstHome(),
+      '/second': (context) => SecondHome(),
+    }
+  ),);
+}
+class User {
+  final String name;
+  final int age;
+
+  User({ this.name, this.age });
+}
+
+class FirstHome extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            User user = User(name: 'Marfa', age: 32);
+            Navigator.pushNamed(context, '/second', arguments: user);
+          },
+          child: Text('Ouuuu home'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondHome extends StatelessWidget {
+  User user;
+
+  @override
+  Widget build(BuildContext context) {
+    RouteSettings settings = ModalRoute.of(context).settings;
+    user = settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${this.user.name} - ${this.user.age}'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
